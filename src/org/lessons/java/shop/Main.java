4ca857b2c0b0a380977c36718d14a4cf7ea46c93
francescotrudu <francescotrudu84@gmail.com>
2023-05-08 17:31:07 +0200

    Delete random

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,11 +1,7 @@
 package org.lessons.java.shop;
-
-import java.util.Random;
-
 public class Main {
 
 	public static void main(String[] args) {
-		Random rnd = new Random();
 		Prodotto product1 = new Prodotto("Iphone", "Description one", 200, 22);
 		Prodotto product2 = new Prodotto("Samsung", "Description two", 100, 22);
 
